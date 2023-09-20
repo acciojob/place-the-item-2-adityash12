a38cd124f165d86181fb88107aacdc12d8d8beb0
adityash12 <128187345+adityash12@users.noreply.github.com>
2023-09-21 01:00:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your code here
+#div1{
+	width:200px;
+	height:200px;
+background-color:#008001;
+	border:2px solid red;
+	
+}
+//your code here
+#div2{
+	width:200px;
+	height:200px;
+	background:#ff0000;
+}
+//your code here
+#div3{
+	width:200px;
+	height:200px;
+	background:#0000ff
+	
+}
+//your code here
+div5{
+	width:200px;
+	height:200px;
+	
+}
+//your code here
+div4{
+	width:200px;
+	height:200px;
+	
+}
